1c17f6fdad456cb705df40eec82ed01a607f3e38
Maggie <mengtian.thu@gmail.com>
2023-05-16 22:47:52 -0700

    fix typo in group model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(
         models.Group,
         {
-          foreignKey: 'orgainzerId'
+          foreignKey: 'organizerId'
         }
       );
     }
