36b4aec659c72dadaf46a823cdeab903dd7b2bfc
Maggie <mengtian.thu@gmail.com>
2023-05-18 08:31:52 -0700

    get all groups endpoint

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Op } = require('sequelize');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { Group } = require('../../db/models');
+const { Group, GroupImage, Membership, sequelize } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -12,9 +12,61 @@ const router = express.Router();
 // Middlewares:
 
 // Route handlers:
+
+// Get all groups:
+// createdAt and updatedAt format, fn doesn't overwrite column, has to rename.
+// count members?
 router.get('/', async (req, res, next) => {
-    const groups = await Group.findAll();
-    return res.json(groups);
+    const groups = await Group.findAll({
+        attributes: {
+            exclude: ['createdAt', 'updatedAt'],
+            include: [
+                [sequelize.fn("strftime", '%Y-%m-%d %H:%M:%S', sequelize.col("createdAt")), 'createdAt1'],
+                [sequelize.fn("strftime", '%Y-%m-%d %H:%M:%S', sequelize.col("updatedAt")), "updatedAt1"]
+            ],
+        }
+    });
+
+    const result = [];
+
+    for (let group of groups) {
+        groupData = group.toJSON();
+
+        // get aggregate: numMembers
+        const numMembers = await Membership.count({
+            where: {
+                groupId: group.id,
+                status: 'member'
+            }
+        });
+
+        // get previewImage
+        const previewImage = await GroupImage.findOne({
+            where: {
+                groupId: group.id,
+                preview: true
+            }
+        });
+
+        // add correct date format, numMembers, previewImage
+        groupData.createdAt = groupData.createdAt1;
+        groupData.updatedAt = groupData.updatedAt1;
+        delete groupData.createdAt1;
+        delete groupData.updatedAt1;
+
+        groupData.numMembers = numMembers;
+
+        if (previewImage) {
+            groupData.previewImage = previewImage.url;
+        }
+        else {
+            groupData.previewImage = `No preview Image for this group`;
+        }
+
+        result.push(groupData);
+    };
+
+    return res.json({ Groups: result });
 });
 
 module.exports = router;
